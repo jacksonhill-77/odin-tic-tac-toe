250204504c3d8c5aff9fe37133c0c28c057466f4
jacksonhill-77 <contactjacksonhill@gmail.com>
2024-03-17 18:49:01 +1100

    Add board and player factories, square marking methods

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const gameBoard = function gameBoard() {
     let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    const markSquare = function(squareToMark, marking) {
-        board[squareToMark] = marking;
+    const markSquare = function(squareToMark, symbol) {
+        board[squareToMark] = symbol;
     }
 
-    return { board, markSquare };
+    function player() {
+        const promptPlayerName = prompt('Please type your player name: ');
+        let symbol = prompt('Please choose whether you want to play as noughts or crosses by entering n or c: ');
+        symbol == 'n' ? symbol = 1 : symbol = 2; 
+    
+    
+        const chooseSquareToMark = function() {
+            let squareToMark = prompt('Please choose which square you want to mark (top left = 1, bottom right = 9): ');
+            squareToMark = +squareToMark - 1;
+            markSquare(squareToMark, symbol);
+            console.log(board);
+        }
+
+        return { chooseSquareToMark }
+    }
+
+    return { board, player };
 }
 
+
+const game = gameBoard();
+const player1 = game.player();
+player1.chooseSquareToMark()
